33901559c072e12d27e9efae184b5ae0a59a8521
Niels van Velzen <git@ndat.nl>
2020-03-08 17:56:41 +0100

    Update to Jellyfin 10.5.0-pre1

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,2 @@
 dotnet publish
-Copy-Item ".\bin\Debug\netcoreapp2.1\Jellyfin.Plugin.Vgmdb.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\netcoreapp3.1\Jellyfin.Plugin.Vgmdb.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
