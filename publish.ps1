d7a218985d7107830691705806a1af02f495f8c0
Niels van Velzen <git@ndat.nl>
2020-03-14 13:46:41 +0100

    Update to netstandard2.1

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
 dotnet publish
-Copy-Item ".\bin\Debug\netcoreapp3.1\Jellyfin.Plugin.Vgmdb.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\netstandard2.1\Jellyfin.Plugin.Vgmdb.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\netstandard2.1\Jellyfin.Plugin.Vgmdb.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
