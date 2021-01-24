cb2d7a869b4c9f1507ad8062fff66c2adcbec2a5
Niels van Velzen <git@ndat.nl>
2021-01-24 17:15:50 +0100

    Update publish.ps1 to use it's own directory

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,4 @@
 dotnet publish
-Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Vgmdb.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
-Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Vgmdb.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+New-Item -ItemType Directory -Force -Path "C:\ProgramData\Jellyfin\Server\plugins\VGMdb_local\"
+Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Vgmdb.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins\VGMdb_local\"
+Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Vgmdb.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins\VGMdb_local\"
